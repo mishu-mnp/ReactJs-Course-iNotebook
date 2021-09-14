7761535fd0e2e95e0cfeb74c9de45c852735a8ca
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-14 21:32:37 +0530

    Tut45 Done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-    res.json([])
+
+// Creating a User using POST request
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const user = User(req.body)
+    user.save()
+    res.send(req.body)
 })
 
 module.exports = router;
