de5e930f46617fb135bde57a0226bc91a7ee9b08
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-17 22:24:11 +0530

    Tut53 Done

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -11,6 +11,8 @@ const fetchuser = (req, res, next) => {
 
     try {
         const data = jwt.verify(token, JWT_SECRET);
+        // console.log(data);
+        // console.log(data.user);
         req.user = data.user;
         next();
 
