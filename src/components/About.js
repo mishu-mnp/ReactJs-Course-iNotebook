22902cf2dbfeb70f83268e36cbdefe9f7636aa9c
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-18 15:47:44 +0530

    Tut59 Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,10 @@
-import React, { useContext, useEffect } from 'react';
-import noteContext from '../context/notes/noteContext';
+import React from 'react';
 
 const About = () => {
-    const a = useContext(noteContext);
-
-    useEffect(() => {
-        a.update();
-        // eslint-disable-next-line
-    }, [])
 
     return (
         <div>
-            <h2>This is About Page of {a.state.name} from class {a.state.div} </h2>
+            <h2>This is About Page </h2>
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,28 +1,10 @@
-import { useState } from "react";
 import NoteContext from "./noteContext";
 
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "vaibhav",
-        "div": "5A2_CSE"
-    }
-
-    const [state, setState] = useState(s1)
-
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "mishu",
-                "div": "5A21_CSE"
-            })
-        }, 1000);
-    }
-
-
 
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
