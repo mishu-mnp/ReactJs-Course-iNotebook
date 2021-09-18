1d2b5617fc365239508f83062604e6c22fe4867d
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-18 16:04:49 +0530

    Tut60 Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import noteContext from '../context/notes/noteContext';
 
 const Home = () => {
+
+    const context = useContext(noteContext)
+    const { notes, setNotes } = context;
     return (
         <>
             <div className="container my-3">
@@ -23,6 +27,9 @@ const Home = () => {
                 </form>
                 <div className="container my-3">
                     <h2>Your Notes</h2>
+                    {notes.map((note) => {
+                        return note.title
+                    })}
                 </div>
             </div>
         </>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
-
 const NoteState = (props) => {
+    const initialNotes = [
+        {
+            "_id": "61443db10a772b6c6f3fde2e",
+            "user": "61430a43f9f7be61d8789cb6",
+            "title": "ReactJs",
+            "description": "Learning React from CWH",
+            "tag": "YouTube",
+            "date": "1631862193944",
+            "__v": 0
+        },
+        {
+            "_id": "61443db10a772b6c6f3fde2e",
+            "user": "61430a43f9f7be61d8789cb6",
+            "title": "NodeJS",
+            "description": "Learning NodeJs from CWH",
+            "tag": "YouTube",
+            "date": "1631862193944",
+            "__v": 0
+        }
+    ]
+
+    const [notes, setNotes] = useState(initialNotes)
 
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{ notes, setNotes }}>
             {props.children}
         </NoteContext.Provider>
     )
